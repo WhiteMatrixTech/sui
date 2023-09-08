d3a44f8e4bcd0d1e164ed3a35f07fe5d12111ef3
Nikhil Mattu <nikhil@mystenlabs.com>
2023-09-08 01:19:52 -0400

    [wallet-ext] forgot password flow

--- apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
+++ apps/wallet/src/ui/app/pages/accounts/ProtectAccountPage.tsx
@@ -33,6 +33,7 @@ export function ProtectAccountPage() {
 	const [searchParams] = useSearchParams();
 	const accountType = searchParams.get('accountType') || '';
 	const successRedirect = searchParams.get('successRedirect') || '/tokens';
+	const isResetting = Boolean(searchParams.get('reset')) || false;
 	const navigate = useNavigate();
 	const { data: accounts } = useAccounts();
 	const createMutation = useCreateAccountsMutation();
@@ -46,7 +47,7 @@ export function ProtectAccountPage() {
 			typeof hasPasswordAccounts !== 'undefined' &&
 			!(createMutation.isSuccess || createMutation.isLoading)
 		) {
-			setShowVerifyPasswordView(hasPasswordAccounts);
+			setShowVerifyPasswordView(hasPasswordAccounts && !isResetting);
 		}
 	}, [hasPasswordAccounts, createMutation.isSuccess, createMutation.isLoading]);
 	const createAccountCallback = useCallback(
@@ -75,6 +76,8 @@ export function ProtectAccountPage() {
 	if (!isAllowedAccountType(accountType)) {
 		return <Navigate to="/" replace />;
 	}
+
+	console.log(showVerifyPasswordView === null && !isResetting);
 	return (
 		<div className="rounded-20 bg-sui-lightest shadow-wallet-content flex flex-col items-center px-6 py-10 overflow-auto w-popup-width h-popup-height">
 			<Loading loading={showVerifyPasswordView === null}>
@@ -97,8 +100,9 @@ export function ProtectAccountPage() {
 						<div className="mt-6 w-full grow">
 							<ProtectAccountForm
 								cancelButtonText="Back"
-								submitButtonText="Create Wallet"
+								submitButtonText={isResetting ? 'Reset Password ' : 'Create Wallet'}
 								onSubmit={({ password }) => createAccountCallback(password.input, accountType)}
+								displayToS={!isResetting}
 							/>
 						</div>
 					</>
